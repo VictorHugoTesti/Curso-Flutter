259c5a077d86052c4ce25461029db477fbba4a14
Victor Testi <victortesti06@gmail.com>
2026-03-26 17:03:00 -0300

    enchantment: 14. Variáveis Básicas

--- dart001/dart_basico.dart
+++ dart001/dart_basico.dart
@@ -10,3 +10,22 @@ void main () {
 }
 */
 
+
+//14. Variáveis Básicas
+/*
+void main () {
+  int age = 20;
+  string name = "Victor";
+  double distancia = 13.22;
+
+  print(age);
+  print(name);
+  print(distancia);
+
+  age = 21;
+  name = 'Hugo'
+
+  print(age);
+  print(name);
+}
+*/
