8e598ac135dd726ab3d97897740008c273ec021c
Victor Testi <victortesti06@gmail.com>
2026-03-29 11:28:45 -0300

    enchantment: 24. Introdução as Funções

---
+++ dart001/dart_funcoes.dart
@@ -0,0 +1,16 @@
+
+// 24. Introdução as Funções
+
+void main() {
+  saudacoes();
+}
+
+void saudacoes () {
+  print('Saudações do Victor');
+  print("Seja Bem Vindo!");
+}
+
+
+//25. Retorno de Funções
+
+void agora () {}
