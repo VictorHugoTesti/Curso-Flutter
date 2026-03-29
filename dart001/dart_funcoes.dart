b0dbf6241af2ccd488a4ebc56d5f96d9727789be
Victor Testi <victortesti06@gmail.com>
2026-03-29 11:59:35 -0300

    enchantment: 27. Variaveis Anuláveis

--- dart001/dart_funcoes.dart
+++ dart001/dart_funcoes.dart
@@ -1,14 +1,13 @@
 
-void main() {
-  saudacoes('Victor', sep: '=', mostraragora: false);
-  saudacoes('João', sep: '*');
+// 24. Introdução as Funções
+
+/*
+void main() { 
 
   String a = agora();
   print(a);
   print(agora());
 }
-
-// 24. Introdução as Funções
 
 void saudacoes (String nome, {bool mostraragora = true, String sep = '-'}) {
 
@@ -20,14 +19,44 @@ void saudacoes (String nome, {bool mostraragora = true, String sep = '-'}) {
     print('Agora: ${agora()}');
   }
 }
+*/
 
 // 25. Retorno de Funções
+
 
 String agora () {
   DateTime agora = DateTime.now();
   return agora.toString();
 }
 
+
 // 26. Paramêtros de Funções
 
-//Função saudacoes
+/*Função saudacoes */
+/*
+void main () {
+  saudacoes('Victor', sep: '=', mostraragora: false);
+  saudacoes('João', sep: '*');
+}
+*/
+
+// 27. Variaveis Anuláveis
+
+void main() {
+  saudacoes('victor');
+
+}
+
+void saudacoes (String nome, {bool mostraragora = true, String? cliente,}) {
+
+  print('Saudações do $nome');
+
+  if(cliente != null) {
+      print("Seja Bem Vindo $cliente!");
+
+  }
+
+  if(mostraragora) {
+    print('Agora: ${agora()}');
+  }
+}
