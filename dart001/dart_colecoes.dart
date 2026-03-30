b6b2c69d54bb429c8d06e63f0376521c805865f5
Victor Testi <victortesti06@gmail.com>
2026-03-30 12:07:36 -0300

    enchantment: 33. Variáveis Dinâmicas

---
+++ dart001/dart_colecoes.dart
@@ -0,0 +1,79 @@
+
+//33. Variáveis Dinâmicas
+
+void main() {
+  //var adota um tipo especifico e não é possivel mudar
+  var nome = 'Victor';
+  nome = 'Hugo';
+
+  //nome = 3;
+
+  var idade = 40;
+  idade++;
+
+  //dynamic pode trocar para qualquer tipo quando quiser
+  dynamic variavel = 'Victor';
+  variavel = 3;
+  variavel = true; 
+
+  // Quando não se sabe se o dado é INT ou DOUBLE, ele aceita os 2 Tipos
+  num numero = 1.363636;
+}
+
+void funcao(num numero) {}
+
+
+//34. Listas
+
+
+
+
+//35. Leitura da Documentação
+
+
+
+
+//36. Funções de Listas
+
+
+
+
+//37. Iteração da Lista
+
+
+
+
+//38. Outras Funções da Lista
+
+
+
+
+//39. Listas no Null-Safety
+
+
+
+
+//40. Mapas
+
+
+
+
+//41. Funções de Mapas e Null Assertion
+
+
+
+
+//42. Mapas no Null-Safety
+
+
+
+
+//43. JSON
+
+
+
+
+//44. JSON na Prática
+
+
+
